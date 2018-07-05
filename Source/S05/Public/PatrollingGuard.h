a084418fffaa0a41308a0999cc1fa1a031fcd52d
DESKTOP-HQEKJ1H\Victor <bonapart@live.se>
2018-07-05 11:52:38 +0200

    242. Translating patroling blueprint to c++

--- Source/S05/Public/PatrollingGuard.h
+++ Source/S05/Public/PatrollingGuard.h
@@ -14,6 +14,7 @@ class S05_API APatrollingGuard : public ATP_ThirdPersonCharacter
 {
 	GENERATED_BODY()
 
+public: // TODO remove or create get
 		UPROPERTY(EditInstanceOnly, Category = "Patrol Route")
 		TArray<AActor*> PatrolPointsCPP;
 	
